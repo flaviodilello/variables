7745dc2b2704ba3cf5dc4b943ba5d711faf516eb
flavio dilello <flaviodilello@gmail.com>
2020-02-17 13:04:32 +0100

    repo con los primeros ejercicios de Java

---
+++ 001 - Variables/src/Fase1.java
@@ -0,0 +1,15 @@
+
+public class Fase1 {
+	public static void main(String[]args) {
+		String name="Flavio";
+		String lastName1="Dilello";
+		String lastName2="Pastrana";
+		int day=17;
+		int month=2;
+		int year=2020;
+		System.out.println("My name is: "+lastName1+" "+lastName2+", "+name);
+		System.out.println("Hoy es: "+day+"/"+month+"/"+year);
+	}
+	
+	
+}
